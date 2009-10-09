82978cc184fe11817917f6850dcf0887e61ddee8
Brian Thorndyke <brian@thorndyke.org>
2009-10-08 20:18:34 -0700

    * Added sendmail directive to production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,9 @@ config.action_controller.perform_caching             = true
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = false
+
+# Use sendmail
+config.action_mailer.delivery_method = :sendmail
 
 SITE="http://quadtask.com"
